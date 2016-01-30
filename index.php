c3e8ff13b849a7d9b655dc58ab182918c90bff81
10858010 <dtt1@pct.edu>
2016-01-29 22:48:48 -0500

    initial commit to BWM399 homepage

---
+++ index.php
@@ -0,0 +1,59 @@
+<!doctype html>
+
+<html lang="en">
+
+<head>
+
+<meta charset="utf-8">
+
+<title>BWM 399 - Front End Frameworks</title>
+
+
+<meta name="viewport" content="width=device-width; initial-scale=1; maximum-scale=1">
+
+<link rel="stylesheet" href="css/style.css">
+
+
+</head>
+
+<body>
+<div id="container">
+<header>
+
+<h1><a href="#" id="logo">BWM 399 - Front End Frameworks</a></h1>
+
+<nav>
+<a href="#" id="menu-icon"></a>
+  <ul>
+  	<li><a href='#'>Home</a></li>
+	<li><a href='#'>Assignment #1</a></li>
+    <li><a href='#'>Assignment #2</a></li>
+    <li><a href='#'>Assignment #3</a></li>
+    <li><a href='#'>Assignment #4</a></li>
+    <li><a href='#'>Assignment #5</a></li>
+    <li><a href='#'>Assignment #6</a></li>
+    <li><a href='#'>Assignment #7</a></li>
+  </ul>
+</nav>
+</header>
+
+<section>
+<h1 align="center">Welcome to My Homepage for BWM3399!</h1>
+<img src="images/snail.gif" alt="Respond" />
+
+
+
+
+<a id="inlink" href="images/Intro.jpg"></a>
+</section>
+
+<div class="footer">
+<div id="subnav">
+</div>
+<p>Copyright &copy; 2015 | Designed by Devyn Tucker</p>
+<p><a href="mailto:dtt1@pct.edu"><font size="2">Contact Me</font></a></p>
+</div>
+</div>
+</body>
+
+</html>
